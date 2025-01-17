7d20c45330e4774d97ff7fe344ee1b8bb78b9c12
Seth Purcell <seth@constructor.dev>
2025-01-16 20:43:51 -0800

    fixes plus button image

--- markdoc.config.mjs
+++ markdoc.config.mjs
@@ -1,6 +1,25 @@
-import { defineMarkdocConfig } from '@astrojs/markdoc/config';
+import { defineMarkdocConfig, component } from '@astrojs/markdoc/config';
 import starlightMarkdoc from '@astrojs/starlight-markdoc';
+
 
 export default defineMarkdocConfig({
   extends: [starlightMarkdoc()],
+  tags: {
+    plus: {
+      render: component('./src/components/Plus.astro'),
+      attributes: {
+      },
+    },
+    image: {
+      render: component('./src/components/Image.astro'),
+      attributes: {
+        // Markdoc requires type defs for each attribute.
+        // These should mirror the `Props` type of the component
+        // you are rendering.
+        // See Markdoc's documentation on defining attributes
+        // https://markdoc.dev/docs/attributes#defining-attributes
+        filename: { type: String },
+      },
+    }
+  },
 });
